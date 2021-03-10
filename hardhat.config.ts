c7d4cbb91a722644f426c89bd548ce7b1d24c9ee
Philipp Muens <raute1337@gmx.de>
2021-03-10 17:09:52 +0100

    Update Solidity compiler version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,7 +13,9 @@ task('accounts', 'Prints the list of accounts', async (_, hre) => {
 })
 
 const config: HardhatUserConfig = {
-  solidity: '0.7.3',
+  solidity: {
+    version: '0.8.0'
+  },
   paths: {
     sources: 'contracts',
     tests: 'tests',
